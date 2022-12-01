4a6459e26506a7f50db6651fb613393203635e43
joarder97 <masudul.pallab99@gmail.com>
2022-12-01 19:07:22 +0600

    IdentityUserExtended

--- BookStore.DataAccess/Data/ApplicationDbContext.cs
+++ BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace BookStore.DataAccess
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
-        public DbSet<Product> Products{ get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
