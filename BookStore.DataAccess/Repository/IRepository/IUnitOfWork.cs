088ed5c788056ba6472f788c407a90d0e9d999d9
joarder97 <masudul.pallab99@gmail.com>
2022-12-13 18:05:17 +0600

    orderHeader&OrderDetailRepo

--- BookStore.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BookStore.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -8,12 +8,14 @@ namespace BookStore.DataAccess.Repository.IRepository
 {
     public interface IUnitOfWork
     {
-        ICategoryRepository Category { get; }
+        IOrderHeaderRepository Category { get; }
         ICoverTypeRepository CoverType { get; }
         IProductRepository Product { get; }
         ICompanyRepository Company { get; }
         IApplicationUserRepository ApplicationUser { get; }
         IShoppingCartRepository ShoppingCart { get; }
+        IOrderHeaderRepository OrderHeader { get; }
+        IOrderDetailRepository OrderDetail { get; }
         
 
         void Save();
--- BookStore.DataAccess/Repository/OrderDetailRepository.cs
+++ BookStore.DataAccess/Repository/OrderDetailRepository.cs
@@ -9,18 +9,18 @@ using System.Threading.Tasks;
 
 namespace BookStore.DataAccess.Repository
 {
-    public class CategoryRepository : Repository<Category>, IOrderHeaderRepository
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
     {
         private ApplicationDbContext _db;
 
-        public CategoryRepository(ApplicationDbContext db) : base(db) 
+        public OrderDetailRepository(ApplicationDbContext db) : base(db) 
         {
             _db = db;
         }
 
-        public void Update(Category obj)
+        public void Update(OrderDetail obj)
         {
-            _db.Categories.Update(obj);
+            _db.OrderDetails.Update(obj);
         }
     }
 }
